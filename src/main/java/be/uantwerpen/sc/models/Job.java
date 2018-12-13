cae0e47fa80b8dac80d0fd4fa190f493ffce3344
Imre Liessens <imre.liessens@telenet.be>
2018-12-13 14:49:29 +0100

    Change classes to support MaaS New version number

--- src/main/java/be/uantwerpen/sc/models/Job.java
+++ src/main/java/be/uantwerpen/sc/models/Job.java
@@ -1,181 +1,79 @@
 package be.uantwerpen.sc.models;
 
+import be.uantwerpen.sc.models.MyAbstractPersistable;
+
+import javax.persistence.*;
+import java.util.List;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
 /**
- * Job Data Class
+ *  NV 2018
  */
-//Copied from RBackbone
-public class Job
-{
-    /**
-     * ID of Job
-     */
-    private long jobId;
+@Entity
 
-    /**
-     * ID of the start waypoint
-     * TODO: Jobs are more variable than this
-     */
+public class Job extends MyAbstractPersistable<Long> {
+
+
     private long idStart;
-
-    /**
-     * ID of the end  waypoint
-     * TODO: Jobs are more variable than this
-     */
     private long idEnd;
-
-    /**
-     * ID of Vehicle
-     */
     private long idVehicle;
-
-    //Copied from RCore
-    private String jobDescription;
-
-//MAAS
     private String typeVehicle;
     private String Status;
-    /**
-     * Default Constructor
-     * TODO Better
-     */
-    public Job(){}
 
-    /**
-     * Todo jobid
-     * @param jobDescription
-     */
-    public Job(String jobDescription)
-    {
-        this(0L, jobDescription);
+    public Job() {
+
     }
 
-    /**
-     * Create Job with defined ID and description
-     * @param jobId
-     * @param jobDescription
-     */
-    public Job(Long jobId, String jobDescription)
-    {
-        this.jobId = jobId;
-        this.jobDescription=jobDescription;
-    }
-    /**
-     * Model that describes a job request.
-     *
-     * @param jobId ID of the job
-     * @param idStart ID of the start waypoint of the route
-     * @param idEnd ID of the end waypoint of the route
-     * @param idVehicle ID of the vehicle
-     */
-    public Job(Long jobId, Long idStart, Long idEnd, Long idVehicle) {
-        this.jobId = jobId;
+
+    public Job(Long idStart, Long idEnd, Long idVehicle) {
         this.idStart = idStart;
         this.idEnd = idEnd;
         this.idVehicle = idVehicle;
     }
-    /**
-     * Get Job ID
-     * @return ID of Job
-     */
-    public Long getJobId()
-    {
-        return jobId;
+
+    public String toString(){
+        return "[ start: "+idStart+" - end:"+idEnd+" ] vehicle:" + idVehicle;
     }
 
-    /**
-     * Set Job ID
-     * @param jobId ID of Job
-     */
-    public void setJobId(Long jobId)
-    {
-        this.jobId = jobId;
-    }
-
-    //RCore
-    public String getJobDescription()
-    {
-        return jobDescription;
-    }
-
-    //RCore
-    public void setJobDescription(String jobDescription)
-    {
-        this.jobDescription = jobDescription;
-    }
-    /**
-     * Get Start waypoint ID
-     * @return Start Point ID
-     */
-    public Long getIdStart()
-    {
+    public long getIdStart() {
         return idStart;
     }
 
-    /**
-     * Set Start waypoint ID
-     * @param idStart Start Point ID
-     */
-    public void setIdStart(Long idStart)
-    {
+    public void setIdStart(long idStart) {
         this.idStart = idStart;
     }
 
-    /**
-     * Get End waypoint ID
-     * @return End Point ID
-     */
-    public Long getIdEnd()
-    {
+    public long getIdEnd() {
         return idEnd;
     }
 
-    /**
-     * Set End waypoint ID
-     * @param idEnd End Point ID
-     */
-    public void setIdEnd(Long idEnd)
-    {
+    public void setIdEnd(long idEnd) {
         this.idEnd = idEnd;
     }
 
-    /**
-     * Get ID of related Job Vehicle
-     * @return ID of Vehicle
-     */
-    public Long getIdVehicle()
-    {
+    public long getIdVehicle() {
         return idVehicle;
     }
 
-    /**
-     * Set ID of Job Vehicle
-     * @param idVehicle ID of vehicle
-     */
-    public void setIdVehicle(Long idVehicle)
-    {
+    public void setIdVehicle(long idVehicle) {
         this.idVehicle = idVehicle;
     }
-    //MAAS
+
     public String getTypeVehicle() {
         return typeVehicle;
     }
+
     public void setTypeVehicle(String typeVehicle) {
         this.typeVehicle = typeVehicle;
     }
 
-    //MAAS
     public String getStatus() {
         return Status;
     }
+
     public void setStatus(String status) {
         Status = status;
     }
 
-    @Override
-    public String toString()
-    {
-        return "Job{" +
-                "jobId=" + jobId +
-                '}';
-    }
 }
---
+++ src/main/java/be/uantwerpen/sc/models/MyAbstractPersistable.java
@@ -0,0 +1,16 @@
+package be.uantwerpen.sc.models;
+
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import java.io.Serializable;
+
+/**
+ * NV 2018
+ */
+public class MyAbstractPersistable<Long extends Serializable> extends AbstractPersistable<Long> {
+    @Override
+    public void setId(Long id) {
+        super.setId(id);
+    }
+}
